916288718008281a5ac1a2f46ca774d4e332cd8c
Ruben Alvarez Mosquera <ralvarmo@emeal.nttdata.com>
2023-11-25 20:09:26 +0100

    v0.1.0 - FIRST Commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# awsauditor
-Python Script to Audit AWS resources
+# AWS Auditor
+
+This project wants to solve the problem when an agent wants to audit all AWS resources (many of them without TAGs) and wants to avoid the tedious task of cherry picking them through the administration center like "Resource Groups & Tags Editor" or so.
+
+Another issue with this is the fact that most of the time we cannot use AWS CLI on our machines (VPN, account permissions, etc). 
+
+So I do these steps in two scripts:
+- Bash script: To run the awsCLI consults and exports.
+- A Python script to transform/export a JSON response to a csv format.
+
+The script should work correctly if the following requirements are met:
+
+AWS CLI and Boto3 are configured: Ensure that AWS CLI is installed and configured with the correct credentials, and that Boto3 is installed in your Python environment.
+
+IAM Permissions: Your AWS user must have the necessary permissions to perform the actions the script intends (such as listing Lambda functions, EventBridge rules, etc.).
+
+Error handling: Consider adding error handling to catch and handle possible exceptions, such as connection errors or insufficient permissions.
+
+Otherwise, the script seems well designed for its purpose of listing AWS resources and exporting them to CSV files. The code structure is clear, and the comments and documentation are adequate and helpful.
---
+++ awsauditor.py
@@ -0,0 +1,63 @@
+"""
+--------------------------------------------------------------------------------
+Script Name: AWS Auditor
+Author: Ruben Alvarez Mosquera
+Created: 23/11/2023
+Last Modified: 23/11/2023
+Version: 0.1.0
+
+Description:
+    Este script automatiza la exportación de recursos AWS a archivos CSV.
+    Soporta múltiples servicios como Lambda, Step Functions, EventBridge, etc.
+
+License:
+    Copyright 2023 by Ruben Alvarez Mosquera.
+    Se concede permiso, de forma gratuita, a cualquier persona que obtenga una copia
+    de este software y de los archivos de documentación asociados (el "Software"), para
+    utilizar el Software sin restricción, incluyendo sin limitación los derechos
+    de usar, copiar, modificar, fusionar, publicar, distribuir, sublicenciar, y/o vender
+    copias del Software, y para permitir a las personas a las que se les proporcione el
+    Software hacer lo mismo, siempre que se incluya el siguiente aviso de derechos de autor
+    y este aviso de permiso en todas las copias o partes sustanciales del Software.
+
+Disclaimer:
+    Este script se proporciona "tal cual", sin garantía de ningún tipo, expresa o
+    implícita. El uso de este script es bajo su propio riesgo.
+
+--------------------------------------------------------------------------------
+"""
+
+
+import boto3
+import csv
+
+def export_to_csv(data, filename, headers):
+    with open(filename, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(headers)
+        for row in data:
+            writer.writerow(row)
+
+def list_lambda_functions(lambda_client):
+    functions = lambda_client.list_functions()
+    return [(f['FunctionName'], f['Runtime'], f['MemorySize']) for f in functions['Functions']]
+
+def list_s3_buckets(s3_client):
+    response = s3_client.list_buckets()
+    return [(bucket['Name'],) for bucket in response['Buckets']]
+
+def main():
+    # Crear clientes para cada servicio
+    lambda_client = boto3.client('lambda')
+    s3_client = boto3.client('s3')
+
+    # Listar funciones de Lambda y buckets de S3
+    lambda_functions = list_lambda_functions(lambda_client)
+    s3_buckets = list_s3_buckets(s3_client)
+
+    # Exportar a CSV
+    export_to_csv(lambda_functions, 'lambda_functions.csv', ['FunctionName', 'Runtime', 'MemorySize'])
+    export_to_csv(s3_buckets, 's3_buckets.csv', ['BucketName'])
+
+if __name__ == '__main__':
+    main()
